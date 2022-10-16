79b63a717d9257393be52261ee6dd75a578489e3
JuanCruzC97 <juancruzcamacho97@gmail.com>
2022-10-16 09:29:51 -0300

    minor changes and comments

--- representations/utils/datasets.py
+++ representations/utils/datasets.py
@@ -2,31 +2,54 @@ import numpy as np
 import pandas as pd
 
 
-def make_classification_dataset(n_samples, noise, inner_factor, outer_factor, random_state):
+def make_classification_dataset(n_samples, noise=0.1, factor_0=0.5, factor_1=0.2, random_state=1):
+    """Esta función genera un dataset de clasificación binaria donde cada clase tiene
+    forma circular. Se puede definir el número de observaciones, el nivel de ruido y 
+    los radios de ambas clases con los parámetros factor.
+
+    Args:
+        n_samples (int): Número de observaciones totales del set de datos generado.
+        noise (float, optional): Nivel de ruido en los datos. Defaults to 0.1.
+        factor_0 (float, optional): Factor que define la magnitud de los valores de la clase 0. Defaults to 0.5.
+        factor_1 (float, optional): Factor que define la magnitud de los valores de la clase 1. Defaults to 0.2.
+        random_state (int, optional): Semilla para números aleatorios. Defaults to 1.
+
+    Returns:
+        pd.DataFrame: Set de datos con 2 columnas de variables explicativas 'X1' y 'X2'
+        y una variable respuesta binaria 'y'.
+    """
     
+    # Cada clase tendrá la mitad del número de observaciones definidos.
+    # En caso de elegir un número impar tendremos un número de observaciones mayor que n_samples.
     n_samples_per_class = int(n_samples/2) 
-   
+    
+    # Generamos los valores para cada clase.
     np.random.seed(random_state)
-    samples_in = np.random.uniform(0, 2 * np.pi, size=n_samples_per_class)
-    samples_out = np.random.uniform(0, 2 * np.pi, size=n_samples_per_class)
+    samples_0 = np.random.uniform(0, 2 * np.pi, size=n_samples_per_class)
+    samples_1 = np.random.uniform(0, 2 * np.pi, size=n_samples_per_class)
     
-    x_in = np.cos(samples_in) * inner_factor
-    y_in = np.sin(samples_in) * inner_factor
-    x_out = np.cos(samples_out) * outer_factor
-    y_out = np.sin(samples_out) * outer_factor
+    # Generación de los valores en x e y de cada punto de cada clase transformados.
+    x1_0 = np.cos(samples_0) * factor_0
+    x2_0 = np.sin(samples_0) * factor_0
+    x1_1 = np.cos(samples_1) * factor_1
+    x2_1 = np.sin(samples_1) * factor_1
     
-    x1 = np.append(x_in, x_out)
+    # Creamos las columnas y agregamos el ruido aleatorio.
+    x1 = np.append(x1_0, x1_1)
     x1 += np.random.normal(0, noise, n_samples_per_class*2)
     
-    x2 = np.append(y_in, y_out)
+    x2 = np.append(x2_0, x2_1)
     x2 += np.random.normal(0, noise, n_samples_per_class*2)
     
+    # Generamos la variable respuesta binaria.
     target = np.append(np.zeros(n_samples_per_class, dtype=int), np.ones(n_samples_per_class, dtype=int))
     
+    # Creamos el dataset de output.
     df = pd.DataFrame({"X1":x1,
                        "X2":x2,
                        "y":target})
     
+    # Definimos a la variable respuesta como variable categórica.
     df = df.astype({"y":"category"})
     
     return df
