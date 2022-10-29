3a3b15dae206bc9ba9880093258546475c7fdbb2
JuanCruzC97 <juancruzcamacho97@gmail.com>
2022-10-29 15:22:59 -0300

    preds functions minor update

--- computer-vision/utils/predictions.py
+++ computer-vision/utils/predictions.py
@@ -12,16 +12,19 @@ def prediction_to_class(x):
 
 
 
-def model_evaluation(model, X_train, y_train, X_val, y_val, X_test, y_test, result=False):
-  eval = dict(train = model.evaluate(X_train, y_train, verbose=0, return_dict=True, batch_size=32),
-              val = model.evaluate(X_val, y_val, verbose=0, return_dict=True, batch_size=32),
-              test = model.evaluate(X_test, y_test, verbose=0, return_dict=True, batch_size=32))
+def model_evaluation(model, X_train, y_train, X_val=None, y_val=None, X_test=None, y_test=None, result=False):
+    
+    #train_eval = model.evaluate(X_train, y_train, verbose=0, return_dict=True, batch_size=32)
+    
+    eval = dict(train = model.evaluate(X_train, y_train, verbose=0, return_dict=True, batch_size=32),
+                val = model.evaluate(X_val, y_val, verbose=0, return_dict=True, batch_size=32),
+                test = model.evaluate(X_test, y_test, verbose=0, return_dict=True, batch_size=32))
 
-  df_eval = pd.DataFrame(eval)
-  display(df_eval)
+    df_eval = pd.DataFrame(eval)
+    display(df_eval)
 
-  if result:
-    return df_eval
+    if result:
+        return df_eval
 
 
 def display_confusion_matrix(y_true, y_pred):
